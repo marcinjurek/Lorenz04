39be357a6418934ff7ce448bb3543d7313c1f28d
Will Boyles <wboyles@tamu.edu>
2019-12-05 13:54:22 -0600

    Start Roxygen documentation

--- R/Lorenz.R
+++ R/Lorenz.R
@@ -1,7 +1,23 @@
 # Rfile to export Lorenz functions from cpp without allowing access to those functions
 # And with wrapping to check compatability
 
-Lorenz04M2 = function(X0, F_Lor, K_Lor, dt, M){
+#' Title
+#'
+#' @param X0 The vector of points you want to pass through a Lorenz Chaos Model.
+#' @param F_Lor A Forcing constant for the Chaos Model. Recommended values are
+#' @param K_Lor 
+#' @param dt 
+#' @param M 
+#'
+#' @return The function returns a new vector that represents a single step in a Lorenz chaos
+#' model as described in the paper above
+#' @export
+#'
+#' @examples
+#' X0 = rnorm(960)
+#' Lorenz04M2(X0, 10, 32, 0.005, 40)
+#' 
+Lorenz04M2 = function(X0, F_Lor, K_Lor, dt = 0.005, M = 40){
   N_Lor = length(X0)
   
   if(!is.vector(X0)){ stop("X0 should be a vector")}
