e8845d948f5cda7316f2bed79777a60f6e0e0c0e
Will Boyles <wboyles@tamu.edu>
2019-12-05 13:03:19 -0600

    Add Lorenz Model c++ function file from research

---
+++ src/Lorenz.cpp
@@ -0,0 +1,173 @@
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+using namespace Rcpp;
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp 
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
+
+// [[Rcpp::export]]
+int mod_cpp(const int& i, const int& n){ 
+  int mod = (i - 1) % n;
+  if(mod < 0)
+  { mod += n; }
+  return mod;
+}
+
+// [[Rcpp::export]]
+double Wn_Even_Cpp(const arma::colvec& XX, const int& n, const int& k, const int& N)
+{
+  double K = k;
+  double J = K / 2;
+  
+  double first = XX(mod_cpp(n - J, N)) / (2 * K);
+  double last = XX(mod_cpp(n + J, N)) / (2 * K);
+  
+  double sums = first + last - XX(mod_cpp(n, N)) / K;
+  
+  for(int i = 0; i < J; ++i)
+  {
+    sums = sums + XX(mod_cpp(n - i, N)) / K + XX(mod_cpp(n + i, N)) / K;
+  }
+  
+  return(sums);
+}
+
+
+double Wn_Odd_Cpp(const arma::colvec& XX, const int& n, const int& k, const int& N){
+  double K = k;
+  double J = (K - 1) /2;
+  double sums = - XX(mod_cpp(n, N)) / K;
+  
+  for(int i = 0; i <= J; ++i)
+  {
+    sums = sums + XX(mod_cpp(n - i, N)) / K + XX(mod_cpp(n + i, N)) / K;
+  }
+  
+  return(sums);
+}
+
+// [[Rcpp::export]]
+double XX_Kn_Even_Cpp(const arma::colvec& XX, const int& n, const int& k, 
+                      const int& N)
+{
+  double K = k;
+  double J = K / 2;
+  
+  double first = (Wn_Even_Cpp(XX, mod_cpp(n - K - J, N), K, N) * XX(mod_cpp(n + K - J, N))) / (2 * K);
+  double last = (Wn_Even_Cpp(XX, mod_cpp(n - K + J, N), K, N) * XX(mod_cpp(n + K + J, N))) / (2 * K);
+  
+  double r_sum = first + last + (Wn_Even_Cpp(XX, mod_cpp(n - K, N), K, N) * XX(mod_cpp(n + K, N)) / K);
+  
+  for(int i = 1; i < J; ++i){
+    r_sum = r_sum + Wn_Even_Cpp(XX, mod_cpp(n - K - i, N), K, N) * XX(mod_cpp(n + K - i, N)) / K +
+            Wn_Even_Cpp(XX, mod_cpp(n - K + i, N), K, N) * XX(mod_cpp(n + K + i, N)) / K;
+    //std::cout << "iteration " << i << ": " << r_sum << std::endl;
+  }
+  
+  double XX_Kn_val = (-Wn_Even_Cpp(XX, mod_cpp(n - (2 * K), N), K, N)) * Wn_Even_Cpp(XX, mod_cpp(n - K, N), K, N) + r_sum;
+  //std::cout << (-Wn_Even_Cpp(XX, mod_cpp(n - (2 * K), N), K, N)) * Wn_Even_Cpp(XX, mod_cpp(n - K, N), K, N) << std::endl;
+  return XX_Kn_val;
+}
+
+
+double XX_Kn_Odd_Cpp(const arma::colvec& XX, const int& n, const int& k, const int& N){
+  
+  double K = k;
+  double J = (K - 1) / 2;
+  
+  double r_sum = (Wn_Odd_Cpp(XX, mod_cpp(n - K, N), K, N) * XX(mod_cpp(n + K, N)) / K);
+  
+  for(int i = 1; i <= J; ++i){
+    r_sum = r_sum + Wn_Odd_Cpp(XX, mod_cpp(n - K - i, N), K, N) * XX(mod_cpp(n + K - i, N)) / K +
+      Wn_Odd_Cpp(XX, mod_cpp(n - K + i, N), K, N) * XX(mod_cpp(n + K + i, N)) / K;
+    //std::cout << "iteration " << i << ": " << r_sum << std::endl;
+  }
+  
+  double XX_Kn_val = (-Wn_Odd_Cpp(XX, mod_cpp(n - (2 * K), N), K, N)) * Wn_Odd_Cpp(XX, mod_cpp(n - K, N), K, N) + r_sum;
+  
+  return XX_Kn_val;
+  
+}
+
+// [[Rcpp::export]]
+arma::colvec Lorenz04M2Cpp(const arma::colvec& X0, const int & F_Lor, const int& K_Lor,
+                           const double& dt, const int& M, const int& N_Lor)
+{
+  // Create vector to return with passed vector
+  arma::colvec XX = X0;
+  // Initialize vector of zeros that will "update" XX
+  arma::colvec dx(N_Lor);
+  dx.fill(0);
+  double F = F_Lor;
+  
+  if(K_Lor % 2 == 0){
+    // Nested for loops to update XX
+    for(int i = 0; i < M ; ++i){
+      for(int j = 0; j < N_Lor; ++j){
+        dx(j) = (XX_Kn_Even_Cpp(XX, j, K_Lor, N_Lor) - XX(j) + F) * dt;
+        //std::cout << dx << std::endl;
+      }
+      
+      XX = XX + dx;
+      //std::cout << XX << std::endl;
+      
+      
+    }
+  }
+  else
+  {
+    // Nested for loops to update XX
+    for(int i = 0; i < M ; ++i){
+      for(int j = 0; j < N_Lor; ++j){
+        dx(j) = (XX_Kn_Odd_Cpp(XX, j, K_Lor, N_Lor) - XX(j) + F) * dt;
+        //std::cout << dx << std::endl;
+      }
+      
+      XX = XX + dx;
+      //std::cout << XX << std::endl;
+      
+      
+    }
+  }
+  
+  
+  
+  return XX;
+}
+
+
+// [[Rcpp::export]]
+arma::mat Lorenz04M2SimCpp(const arma::colvec& Xinit, const int& F_Lor, const int& K_Lor,
+                        const double& dt, const int& M, const int& iter, const int& burn)
+{
+  // Get N_Lor from Xinit
+  int N_Lor = Xinit.n_rows;
+  // Create Matrix to store generated data
+  arma::mat Xiter(N_Lor, iter);
+  
+  // Create vector to allow for burn-in iterations
+  arma::colvec newXburn = Xinit;
+  
+  for(int i = 0; i < burn; ++i){
+    newXburn = Lorenz04M2Cpp(newXburn, F_Lor, K_Lor, dt, M, N_Lor);
+  }
+  
+  Xiter.col(0) = Lorenz04M2Cpp(newXburn, F_Lor, K_Lor, dt, M, N_Lor);
+  
+  for(int i = 1; i < iter; ++i){
+    Xiter.col(i) = Lorenz04M2Cpp(Xiter.col(i - 1), F_Lor, K_Lor, dt, M, N_Lor);
+  }
+  
+  
+  return Xiter;
+}
+
+
+
