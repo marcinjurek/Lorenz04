7b60d1f7f816594eea15a3908728c976f20bcdd6
Will Boyles <wboyles@tamu.edu>
2019-12-06 11:25:36 -0600

    Edit N=480 documentation for less iterations

--- R/data.R
+++ R/data.R
@@ -1,13 +1,55 @@
 #' Lorenz Model N = 480
 #' 
-#' 1000 simulations after a burn-in of 200 from a Lorenz04M2 Model with N = 480, F_Lor = 10, K_Lor = 16,
+#' 500 simulations after a burn-in of 200 from a Lorenz04M2 Model with N = 480, F_Lor = 10, K_Lor = 16,
 #' dt = 0.005, and M = 40. Figure 4(b) gives example of what this model is supposed to
 #' look similar to, up to 960 locations.
 #'  
-#' @format A matrix with 1000 columns for the simulations and 480 rows for the locations
+#' @format A matrix with 500 columns for the simulations and 480 rows for the locations
 #'
 #'
 #' @source \href{https://journals.ametsoc.org/doi/pdf/10.1175/JAS3430.1}{Lorenz (2005)}
 #'
 "Lorenz480"
 
+#'
+#'
+#'
+#'
+#'
+#'
+"x.mult.prior"
+
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+"y.mult.i"
+
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+"H.mat"
+
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+"tau.mat"
+
+#'
+#'
+#'
+#'
+#'
+#'
+"S2d.ord"
