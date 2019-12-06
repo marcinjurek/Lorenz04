68688bae8aabca25619031d6e53ff87487bb2e46
Will Boyles <wboyles@tamu.edu>
2019-12-06 16:04:06 -0600

    Get rid of unneeded ordering function

--- R/VecchiaFunctions.R
+++ R/VecchiaFunctions.R
@@ -68,29 +68,6 @@ samp_posts <- function(posts, NNarray, m = m) {
   return(uhat)
 }
 
-# Gets
-orderMaxMinFast <- function( locs, numpropose ){
-  # Get dims
-  n <- nrow(locs)
-  d <- ncol(locs)
-  remaininginds <- 1:n
-  orderinds <- rep(0L,n)
-  # pick a center point
-  mp <- matrix(colMeans(locs),1, d)
-  distmp <- fields::rdist(locs,mp)
-  ordermp <- order(distmp)
-  orderinds[1] = ordermp[1]
-  remaininginds <- remaininginds[remaininginds!=orderinds[1]]
-  for( j in 2:(n-1) ){
-    randinds <- sample(remaininginds,min(numpropose,length(remaininginds)))
-    distarray <-  fields::rdist(locs[orderinds[1:j-1],,drop=FALSE],locs[randinds,,drop=FALSE])
-    bestind <- which(matrixStats::colMins(distarray) ==  max( matrixStats::colMins( distarray ) ))
-    orderinds[j] <- randinds[bestind[1]]
-    remaininginds <- remaininginds[remaininginds!=orderinds[j]]
-  }
-  orderinds[n] <- remaininginds
-  orderinds
-}
 
 
 init_txx2 <- function(data, NNarray) {
